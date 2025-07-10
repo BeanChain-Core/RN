249917a1d7be54f0ce281f3039d564ef4836540f
Non Fasho <samfawk@limabean.xyz>
2025-07-09 20:17:45 -0400

    security vis wizcrypt encryption for RN

--- src/main/java/io/beanchain/nodePortal/portal.java
+++ src/main/java/io/beanchain/nodePortal/portal.java
@@ -1,9 +1,15 @@
 package io.beanchain.nodePortal;
 
+import java.io.File;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
+
+import com.beanpack.Wizard.WizCryptHandler;
 import com.beanpack.beanify.Branding;
 import io.beanchain.config.ConfigLoader;
 import io.beanchain.managers.RewardWalletManager;
+import io.beanchain.services.InternalTxFactory;
 import io.beanchain.tools.Node;
 import io.beanchain.RNApplication;
 
@@ -13,10 +19,35 @@ public class portal {
 
     public static void setIsSyncing(boolean bool) {isSyncing = bool;}
 
+    private static final String CONFIG_FOLDER = "config.docs/";
+    private static final String KEY_PATH = CONFIG_FOLDER + "wiz.txt";
+
 
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("[WIZCrypt] Shutting down — decrypting config...");
+            WizCryptHandler.decryptConfig();
+            scanner.close();
+        }));
         System.out.println(Branding.logo);
         ConfigLoader.loadConfig();
+
+        
+        System.out.println("ENTER TEAM PASSWORD");
+        String pass = scanner.nextLine().trim();
+        WizCryptHandler.setPassword(pass);
+        WizCryptHandler.setWizFileEnc("wiz.txt.enc");
+        WizCryptHandler.setConfigFolder(new File("config.docs/"));
+        WizCryptHandler.setKeyPath(KEY_PATH);
+        WizCryptHandler.bootWizCrypt(pass);
+
+        String key = WizCryptHandler.readL2EncWizKey();
+        InternalTxFactory.initialize(key);
+
+        WizCryptHandler.encryptConfig();
+       
         RewardWalletManager.initializeBalances();
 
         try {
